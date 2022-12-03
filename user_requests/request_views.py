d506efa69b7b830d001b44e76fdef515c6cab1ae
mohammadbilalniazi <mohammadbilalniazi2016@gmail.com>
2022-12-03 19:07:40 +0500

    make it better

--- user_requests/request_views.py
+++ user_requests/request_views.py
@@ -12,10 +12,10 @@ from django.template import loader
 import json
 from shopapp.models import Log
 from .models import Request
-from .serializer import ServicesSerializer,RequestSerializer
+from .serializer import RequestSerializer
 #from .serializer import ControllerIhsayaSerializer
 import re
-from django.contrib.auth.decorators import login_required,permission_required
+from django.contrib.auth.decorators import login_required
 from rest_framework.decorators import api_view
 from django.conf import settings
 from django.core.mail import send_mail
@@ -25,26 +25,27 @@ def clean(st):
     pattern="[\n\t\s]"
     return re.sub(pattern,"",st)
 
-@login_required(login_url='/')
+# @login_required(login_url='/')
 #@permission_required('hawala.add_kitabkhana',login_url='/admin')
 @api_view(('POST','GET'))
 def request(request): 
     ################################################### end Branch 1###############
+    # print("request.method ",request.method," request.body ",request.body)
     if request.method=="POST":
-        print("request.data=",request.data)
-        print("test1")
+        # print("request.data=",request.data)
+        # print("test1")
         # ######################################################mudeeriath############################
         serializer=RequestSerializer(data=request.data)
         #print("serializer=",serializer)
         if serializer.is_valid():
-            print("serializer.validated_data=",serializer.validated_data)
+            # print("serializer.validated_data=",serializer.validated_data)
             serializer.save()
             subject="السلام علیکم"
             #from_email=settings.EMAIL_HOST_USER
             sender=settings.EMAIL_HOST_USER
             recipient_list=[request.data['requester_email'],]
             recipient=[clean(request.data['requester_email'])]
-            message="Dear Requester. Kindly wait untill we call you for interview"
+            message="Dear Client. Kindly wait untill we call you"
             try:
                 #send_mail(subject=subject,message=message,from_email=from_email,recipient_list=recipient_list)
                 send_mail(subject, message, sender, recipient)
